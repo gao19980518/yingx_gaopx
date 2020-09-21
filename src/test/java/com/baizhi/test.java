a5db4021ec94febfee49cc770bc772f325a087f2
gao19980518 <71572078+gao19980518@users.noreply.github.com>
2020-09-22 07:36:06 +0800

    克隆的提交并推送

--- src/test/java/com/baizhi/test.java
+++ src/test/java/com/baizhi/test.java
@@ -4,5 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println(3 * 0.1 == 0.3);
         System.out.println("第一次本地提交修改");
+        System.out.println("第一次远程修改拉取");
+        System.out.println("克隆的项目本地提交并推送");
     }
 }
