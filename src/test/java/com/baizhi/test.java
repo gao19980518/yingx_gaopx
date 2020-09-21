2f058b4c4514e197397b639df7f9b83de4a6de83
gaopx <2100674930@qq.com>
2020-09-22 07:34:14 +0800

    应学项目本地提交第2版本 v2.0版

--- src/test/java/com/baizhi/test.java
+++ src/test/java/com/baizhi/test.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class test {
     public static void main(String[] args) {
         System.out.println(3 * 0.1 == 0.3);
+        System.out.println("第一次本地提交修改");
     }
 }
