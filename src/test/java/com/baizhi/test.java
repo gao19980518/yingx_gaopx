d3c07e6e46e00e655aed926d5c9d05c6f4a1c401
gao19980518 <71572078+gao19980518@users.noreply.github.com>
2020-09-22 07:36:06 +0800

    Update test.java
    第一次远程修改

--- src/test/java/com/baizhi/test.java
+++ src/test/java/com/baizhi/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println(3 * 0.1 == 0.3);
         System.out.println("第一次本地提交修改");
+        System.out.println("第一次远程修改拉取");
     }
 }
